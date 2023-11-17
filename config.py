507e30baec5e4e5ae2c714131b526fea97ec4e66
Avijr004 <128925724+Avijr004@users.noreply.github.com>
2023-11-17 20:50:08 +0530

    config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "21568806"))
+API_HASH = getenv("API_HASH", "83c41043d5ada58ad3dc95652afa70d5")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "6819381670:AAGHM2b7aJHP_GE0LnXI8elW9odocGopieA")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://avineyjr004:Sindhu@003@cluster0.cqunmib.mongodb.net/?retryWrites=true&w=majority")
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+OWNER_ID = int(getenv("OWNER_ID", "1556830659"))
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/we_are_universee")
